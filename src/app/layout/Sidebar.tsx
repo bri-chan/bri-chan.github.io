4d563f5cd762e6218a81c5fb3893eee996a4a13c
Brianna Chan <brichano@amazon.com>
2023-08-12 14:22:31 -0700

    fix link

--- src/app/layout/Sidebar.tsx
+++ src/app/layout/Sidebar.tsx
@@ -71,7 +71,7 @@ export default function Sidebar({
         <Tooltip title="Source of this project" arrow placement="right">
           <Link
             target="_blank"
-            href={"https://github.com/john7rho/react-vscode-portfolio"}
+            href={"https://github.com/bri-chan/bri-chan.github.io"}
             underline="none"
             color="inherit"
             sx={{ WebkitTapHighlightColor: "rgba(0,0,0,0)" }}
